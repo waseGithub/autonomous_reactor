5b94ea5ffe575e2a1f36b075e3613d936c036e83
harveyrutland <hcrutland@mail.com>
2023-06-05 17:42:56 +0100

    again

--- src/feather_m4/data_to_sql_adalogger.py
+++ src/feather_m4/data_to_sql_adalogger.py
@@ -59,14 +59,14 @@ def resample_max(df, time, cols, round_val):
 
 
 df_auto_control['datetime'] = pd.to_datetime(df_auto_control['datetime'], errors='coerce')
-df_auto_control.set_index(['datetime'], inplace=True)
+df_auto_control.set_index('datetime', inplace=True)
 
-# df_auto_control = df_auto_control.applymap(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
+df_auto_control['A Bus Voltage'] = df_auto_control['A Bus Voltage'].str.replace(' V', '')
+df_auto_control['A Current'] = df_auto_control['A Current'].str.replace(' mA', '')
 
-
-
-df_auto_control['A Bus Voltage'] = df_auto_control['A Bus Voltage'].str.replace(' V', '').astype(float)
-df_auto_control['A Current'] = df_auto_control['A Current'].str.replace(' mA', '').astype(float)
+df_auto_control['A Bus Voltage'] = pd.to_numeric(df_auto_control['A Bus Voltage'], errors='coerce')
+df_auto_control['A Current'] = pd.to_numeric(df_auto_control['A Current'], errors='coerce')
+df_auto_control = df_auto_control.dropna(subset=['A Bus Voltage', 'A Current'])
 
 print(df_auto_control)
 df_auto_control = resample_mean(df_auto_control, '30T', ['A Bus Voltage','A Current', 'Set Voltage','digital intput'], 3)
