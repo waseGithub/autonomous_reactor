934faff4ffe4174f9e88eb307ebb7623472b63f7
harveyrutland <hcrutland@mail.com>
2023-06-20 10:50:33 +0100

    push controll update

--- src/feather_m4/data_to_sql_feedrate.py
+++ src/feather_m4/data_to_sql_feedrate.py
@@ -72,7 +72,7 @@ df['feedrate_ml_D'] = ((df['pump_voltage_V'] * 1.0726) - 0.021)*1440
 
 df['datetime'] = df['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
-df = resample_mean(df, '2T', 3)
+df = resample_mean(df, '2T',['pump_voltage_V'], 3)
 
 
 
