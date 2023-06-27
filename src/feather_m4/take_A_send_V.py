7160886102e9b8bce89ff50294bf9be5e72be2fb
harveyrutland <hcrutland@mail.com>
2023-06-27 12:04:10 +0100

    again

--- src/feather_m4/take_A_send_V.py
+++ src/feather_m4/take_A_send_V.py
@@ -60,7 +60,7 @@ while True:
         
         data_dict['datetime'] = str(datetime.now())
         data_log_time_check = 0.1
-        feedrate_time_check = 3
+        feedrate_time_check = 0.5
         if time_checker1.has_passed_minutes(data_log_time_check):
             time_checker1.reset()
 
